2f891d80da7ddb1adc004e5cf0753cfaf378b2c8
Muturi-002 <martinmnjoroge03@gmail.com>
2025-08-28 02:38:34 +0300

    Tried changes. No success yet

--- bucketStorage/src/main/java/SDK/SDK.java
+++ bucketStorage/src/main/java/SDK/SDK.java
@@ -2,28 +2,45 @@ package SDK;
 
 import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 import com.oracle.bmc.objectstorage.ObjectStorageClient;
-import com.oracle.bmc.objectstorage.model.CreateBucketDetails;
-import com.oracle.bmc.objectstorage.model.ObjectSummary;
+import com.oracle.bmc.objectstorage.model.*;
 import com.oracle.bmc.objectstorage.requests.*;
-import com.oracle.bmc.objectstorage.responses.CreateBucketResponse;
-import com.oracle.bmc.objectstorage.responses.ListBucketsResponse;
-import com.oracle.bmc.objectstorage.responses.ListObjectsResponse;
-import com.oracle.bmc.objectstorage.responses.PutObjectResponse;
+import com.oracle.bmc.objectstorage.responses.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 @RestController
-public class SDK {
+@Component
+public class SDK implements CommandLineRunner {
     //Setting the variables above as static allows them to be used in the SDK classes.
     static String bucketName = "BucketStore-1";
     static ConfigFileAuthenticationDetailsProvider provider;
     static ObjectStorageClient client;
     static ListObjectsResponse osResponse;
 
-    @PutMapping("/api/setBucket")
+    static String bucketPAR= bucketName+"PAR";
+    //Checking most recent PAR expiry
+    static Calendar calendar = Calendar.getInstance();
+    static Date expiryDate ;
+
+    // Initialize
+    @Override
+    public void run(String... args) throws Exception {
+        // Load auth provider from default OCI config file (~/.oci/config). Eases connection to OCI services by using already set credentials.
+        provider = new ConfigFileAuthenticationDetailsProvider("~/.oci/config", "DEFAULT");
+
+        // Create Object Client
+        client =  ObjectStorageClient.builder().build(provider);
+        client.setRegion(provider.getRegion());
+
+        //Create Bucket "BucketStore-1" if not exists
+        setBucket();
+    }
+    @GetMapping("/api/bucketInit")
     private static void setBucket() throws IOException{
         String namespace = client.getNamespace(GetNamespaceRequest.builder().build()).getValue();
         try {
@@ -71,7 +88,7 @@ public class SDK {
         }
     }
 
-    @PostMapping("/api/upload")
+    @PostMapping("/upload")
     public String uploadMedia(@RequestParam("file") MultipartFile file) throws IOException {
         setBucket();
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
@@ -81,10 +98,10 @@ public class SDK {
                 .putObjectBody(file.getInputStream()) // Provide content for the object
                 .build();
         PutObjectResponse response = client.putObject(putObjectRequest);
-        listObjects();
         return "Upload succeeded, ETag: " + response.getETag() +"\n Object name: " + file.getOriginalFilename();
     }
-    @GetMapping("/api/listmedia")
+
+    @GetMapping("/listmedia")
     private List<String> listObjects() throws IOException {
         setBucket();
         String namespace = client.getNamespace(GetNamespaceRequest.builder().build()).getValue();
@@ -117,17 +134,56 @@ public class SDK {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        // Load auth provider from default OCI config file (~/.oci/config). Eases connection to OCI services by using already set credentials.
-       provider = new ConfigFileAuthenticationDetailsProvider("~/.oci/config", "DEFAULT");
+    private static void createPAR(){
+        calendar.add(Calendar.DATE,7);
+        expiryDate= calendar.getTime();
+        CreatePreauthenticatedRequestDetails createPAR= CreatePreauthenticatedRequestDetails.builder()
+                .name(bucketPAR)
+                .bucketListingAction(PreauthenticatedRequest.BucketListingAction.ListObjects)
+                .accessType(CreatePreauthenticatedRequestDetails.AccessType.AnyObjectReadWrite)
+                .timeExpires(expiryDate)
+                .build();
 
-        // Create Object Client
-        client =  ObjectStorageClient.builder().build(provider);
-        client.setRegion(provider.getRegion());
+        CreatePreauthenticatedRequestRequest createPARrequest= CreatePreauthenticatedRequestRequest.builder()
+                .namespaceName(client.getNamespace(GetNamespaceRequest.builder().build()).getValue())
+                .bucketName(bucketName)
+                .createPreauthenticatedRequestDetails(createPAR)
+                .build();
 
-        //Create Bucket "BucketStore-1" if not exists
-        setBucket();
-//        putObjects();
-//        deleteObjects();
+        CreatePreauthenticatedRequestResponse responsePAR= client.createPreauthenticatedRequest(createPARrequest);
     }
+
+    @GetMapping("/bucketpar")
+    private static String getPAR(){
+        ListPreauthenticatedRequestsRequest listPAR = ListPreauthenticatedRequestsRequest.builder()
+                .namespaceName(client.getNamespace(GetNamespaceRequest.builder().build()).getValue())
+                .bucketName(bucketName)
+                .build();
+
+        ListPreauthenticatedRequestsResponse listPARresponse= client.listPreauthenticatedRequests(listPAR);
+        Optional<PreauthenticatedRequestSummary> mostRecentPAR = listPARresponse.getItems().stream()
+                .max(Comparator.comparing(PreauthenticatedRequestSummary::getTimeCreated));
+        PreauthenticatedRequestSummary par = mostRecentPAR.get();
+        if (listPARresponse.getItems().isEmpty()) {
+            System.out.println("No PARs found for bucket `"+bucketName+"`. Creating new PAR....\n");
+            createPAR();
+        }else {
+            Date expiryTime = par.getTimeExpires();
+
+            System.out.println("Most recent PAR found:");
+            System.out.println("Time created: " + par.getTimeCreated());
+            System.out.println("Time expired: " + expiryTime);
+            System.out.println("Current time: " + new Date());
+            System.out.println("-------------");
+
+            // Check if the PAR has expired
+            if (expiryTime.before(new Date())) {
+                System.out.println("This PAR has EXPIRED. You may need to create a new one.");
+                createPAR();
+            }
+        }
+        return mostRecentPAR.get().toString();
+    }
+
+
 }
